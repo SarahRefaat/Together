56244c0ce1c5f80d412a47134dadc4653893fb82
sarah-intake40 <sarahrefaatss@gmail.com>
2020-06-07 15:43:14 +0200

    personal token fixed

--- Together/routes/api.php
+++ Together/routes/api.php
@@ -28,7 +28,7 @@ Route::post('/signin','Api\UserController@signin');
 //-------------------------------this route to view user profile
 Route::get('/show','Api\UserController@show')->middleware('auth:sanctum');
 //-----------------------------------this route to get all intersts
-Route::get('/interests','Api\InterestController@interests');
+Route::get('/interest','Api\InterestController@interests');
 //-----------------------------this route to get all groups with the same interest
 Route::get('/groups/{id}','Api\InterestController@ListGroups');
 //-----------------------------------this to update profile
