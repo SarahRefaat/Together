e8ef279ed1bed3859cfcfc7c5f9378f93ddde6e0
nahla samir <nahla.s1995@gmail.com>
2020-06-15 08:36:21 -0700

    final final update to todo notification isa

--- Together/app/Helpers/Helper.php
+++ Together/app/Helpers/Helper.php
@@ -23,8 +23,8 @@ use Illuminate\Support\Facades\DB;
     static function save_notification_for_todo(Group $group,Task $task,$todo_state)
     {
         $user_ids = DB::table('group_user')
-        ->where(['user_id','<>',$group->admin_id],
-        ['group_id',$group->id])
+        ->where([['user_id','<>',$group->admin_id],
+        ['group_id',$group->id]])
         ->get('user_id');
         foreach ($user_ids as $id)
         {
@@ -42,8 +42,8 @@ use Illuminate\Support\Facades\DB;
     static function save_notification_Task(Group $group,Task $task,$state)
     {
         $user_ids = DB::table('group_user')
-        ->where(['user_id','<>',$group->admin_id],
-        ['group_id',$group->id])
+        ->where([['user_id','<>',$group->admin_id],
+        ['group_id',$group->id]])
         ->get('user_id');
         foreach ($user_ids as $id)
         {
